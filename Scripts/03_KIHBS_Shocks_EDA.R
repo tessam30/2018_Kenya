6abdbc950695b1348e0b5300febf724a5b9eb6ac
tessam30 <tessam30@users.noreply.github.com>
2018-11-15 16:11:02 -0500

    Update 03_KIHBS_Shocks_EDA.R

--- Scripts/03_KIHBS_Shocks_EDA.R
+++ Scripts/03_KIHBS_Shocks_EDA.R
@@ -9,9 +9,18 @@ library(corrr)
 library(robustbase)
 library(srvyr)
 
+
+
+# Define shock datasets to work with --------------------------------------
+
 # Using shocks dataframe created in the 02_KIHBS_Shocks.R file
 glimpse(shocks)
-shocks_geo <- shocks %>%
+shocks_geo <- shocks %>% 
+  mutate(county_id = parse_integer(labelled::to_factor(county, levels = "values"))) %>% 
+  left_join(county_labels)
+
+# Create a parallel data set
+shocks_geo_severe <- shocks_severe %>% 
   mutate(county_id = parse_integer(labelled::to_factor(county, levels = "values"))) %>% 
   left_join(county_labels)
 
@@ -22,6 +31,10 @@ shocks_geo_asal <- shocks %>%
   mutate(count = n()) %>% 
   ungroup %>% 
   left_join(asal, by = c("county_id" = "CID"))
+
+
+
+# How correlated are the different types of shocks ------------------------
 
 # Check how correlated the different types of shocks are within households 
 shocks %>% select(contains("_bin")) %>% cor() %>% reshape2::melt() %>% 
@@ -36,7 +49,7 @@ shocks %>% select(contains("_bin")) %>% cor() %>% reshape2::melt() %>%
   ylab("") +
   xlab("")
 
-# TODO - check how correlations vary at the County level
+# Check how correlations vary at the County level
 # First check how many counties have 0 reported shocks for the sub-categories
 # NOTES: There is a tradeoff between the nest() and split approach
 # nest() worked better here b/c the correlate function works well with a dataframe, rather than a list
@@ -50,15 +63,15 @@ shocks_geo %>%
 corr_geo <- shocks_geo %>% 
   select(contains("_bin"), county_name) %>% 
   group_by(county_name) %>% 
-  nest()%>% 
-  mutate(cor = map(data, ~correlate(.)))%>% 
+  nest() %>% 
+  mutate(cor = map(data, ~correlate(.))) %>% 
   unnest(cor) %>% 
   rename(shock_categ1 = rowname) %>% 
   gather(., key = "shock_categ2", value = "correlation", ag_bin:water_short_bin) %>% 
   mutate_at(vars(shock_categ1, shock_categ2), ~gsub("_shock", "", .)) 
  
 # Plot the results to see how intercorrelated the shocks are at the County level
-  tmp %>% 
+corr_geo %>% 
   ggplot(., aes(x = shock_categ1, y = shock_categ2, fill = correlation)) +
   geom_tile(color = "#ffffff") +
   scale_fill_viridis_c(direction = -1, option = "A") +
@@ -70,7 +83,9 @@ corr_geo <- shocks_geo %>%
            
 
 # Check just the core shocks now
-  shocks_geo %>% 
+# Can toggle between data frames
+# shocks_geo and shocks_geo_severe
+  shocks_geo_severe %>% 
     select(ag_shock:other_shock, county_name) %>% 
     group_by(county_name) %>% 
     nest() %>% 
@@ -94,57 +109,115 @@ corr_geo <- shocks_geo %>%
 # --- USing the srvyr library which is a largely a wrapper for survey
 # Next steps, figure ou the sampling weights and the appropriate use
 # svyset clid [pw = weight], strata(strat) singleunit(centered)
+
+# Check the sample design  
 sample_design <-  svydesign(id = ~clid, strata = ~strat,  weights = ~weight, data = shocks)
 summary(sample_design)
 
-
 # Can also use the srvyr package which takes pipes
-shocks_svy <- shocks_geo_asal %>% 
+# Create two data frames
+# 1) takes the complete range of shocks
+# 2) takes only the most sever shock -- what is reported in KIHBS
+
+shocks_geo_asal_svy <- shocks_geo_asal %>% 
   group_by(county_id) %>% 
-  as_survey_design(id = clid, strata = strat,  weights = weight)
+  as_survey_design(id = clid, strata = strat,  weights = weight) 
+
+shocks_geo_severe_svy <- shocks_geo_severe %>% 
+  as_survey_design(id = clid, strata = strat, weights = weight)
+
+# Doesn't make sense to write a function b/c of summarise_all 
+# Calculate overall summary statistics for both datasets
+shocks_stats_natl <- 
+  shocks_geo_asal_svy %>% 
+  select(contains("_shock"), - total_shocks, anyshock) %>% 
+  summarise_all(survey_mean) %>% 
+  select(-contains("_se")) %>% 
+  gather(., key = shock, value = value) %>% 
+  mutate(shock_natl = as_factor(gsub("_shock", "", shock))) %>% 
+  arrange(desc(value)) %>% 
+  select(-shock)
+
+shock_stats_county <- 
+  shocks_geo_asal_svy %>% 
+  select(contains("_shock"), - total_shocks, anyshock, county_name, county_id) %>% 
+  group_by(county_name, county_id) %>% 
+  summarise_all(survey_mean)%>% 
+  select(-contains("_se"))%>% 
+  gather(., key = shock, value = value, ag_shock:anyshock)%>% 
+  mutate(shock = as_factor(gsub("_shock", "", shock))) %>% 
+  left_join(shocks_stats_natl, by = c("shock" = "shock_natl")) %>%
+  rename(value_county = value.x, value_natl = value.y) %>% 
+  mutate(shock_dev = value_county - value_natl)
 
 
-# Calculating statistics by county
-shocks_county <- shocks_svy %>% 
-  group_by(county_id, county_name, Category) %>% 
-  summarise(ag = survey_mean(ag_shock), 
-            conflict = survey_mean(conflict_shock),
-            hazard = survey_mean(hazard_shock),
-            financial = survey_mean(financial_shock),
-            health = survey_mean(health_shock),
-            price = survey_mean(price_shock),
-            anyshock = survey_mean(anyshock))
+# Plot the deviations and levels ------------------------------------------
 
-shocks_asal <- shocks_svy %>% 
-  group_by(Category) %>% 
-  summarise(ag = survey_mean(ag_shock), 
-            conflict = survey_mean(conflict_shock),
-            hazard = survey_mean(hazard_shock),
-            financial = survey_mean(financial_shock),
-            health = survey_mean(health_shock),
-            price = survey_mean(price_shock),
-            anyshock = survey_mean(anyshock))
 
- counties_geo %>% 
-   right_join(shocks_county, by = c("CID" = "county_id")) %>% 
-   select(-contains("_se")) %>% 
-   gather(., key = shock, value = value, ag:anyshock) %>% 
-   ggplot() +
-   geom_sf(aes(fill = value), colour = "white", size = 0.5) +
-   scale_fill_viridis_c(option = "A", direction = -1) +
-   facet_wrap(~shock, nrow = 2) 
-   
+# Map the deviations
+counties_geo %>% 
+  right_join(shock_stats_county, by = c("CID" = "county_id")) %>% 
+  mutate(max = abs(max(shock_dev)),
+         shock = fct_reorder(shock, value_natl, .desc = TRUE)) %>% 
+  ggplot() +
+  geom_sf(aes(fill = shock_dev), colour = "white", size = 0.5) +
+  scale_fill_distiller(type = "div", palette = "PiYG",
+                       limits = c(-0.65, 0.65)) +
+  facet_wrap(~shock, nrow = 2) +
+  ggtitle("Deviations from national average, by shock")
+
+
+counties_geo %>% 
+  right_join(shock_stats_county, by = c("CID" = "county_id")) %>% 
+  mutate(max = abs(max(shock_dev)),
+         shock = fct_reorder(shock, value_natl, .desc = TRUE), 
+         county_name = fct_reorder(county_name, value_county)) %>% 
+  ggplot(aes(x = county_name, y = shock_dev, fill = shock_dev)) +
+  geom_col() +
+  coord_flip()+
+  scale_fill_distiller(type = "div", palette = "PiYG",
+                     limits = c(-0.65, 0.65))+
+  facet_wrap(~shock, nrow = 2)
+
+# Now the levels
+counties_geo %>% 
+  right_join(shock_stats_county, by = c("CID" = "county_id")) %>% 
+  mutate(shock = fct_reorder(shock, value_natl, .desc = TRUE)) %>% 
+  ggplot() +
+  geom_sf(aes(fill = value_county), colour = "white", size = 0.5) +
+  scale_fill_viridis_c(alpha = 0.66, direction = -1, option = "A") +
+  facet_wrap(~shock, nrow = 2) +
+  ggtitle("Shock averages by county")
+
+counties_geo %>% 
+  right_join(shock_stats_county, by = c("CID" = "county_id")) %>% 
+  mutate(max = abs(max(shock_dev)),
+         shock = fct_reorder(shock, value_natl, .desc = TRUE), 
+         county_name = fct_reorder(county_name, value_county)) %>% 
+  ggplot(aes(x = county_name, y = value_county, fill = value_county)) +
+  geom_col() +
+  geom_hline(aes(yintercept = value_natl), colour = "#D3D3D3") +
+  coord_flip() +
+  scale_fill_viridis_c(alpha = 0.66, direction = -1, option = "A") +
+  facet_wrap(~shock, nrow = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
  
 
-
-
-
-
-
-
-svywrangler::calcPtEst(shocks, var = 'anyshock', 
-                       by_var = 'county', use_weights = TRUE, 
-                       psu_var = 'clid', strata_var = 'strat', 
-                       weight_var = 'weight') 
+# svywrangler::calcPtEst(shocks, var = 'anyshock', 
+#                        by_var = 'county', use_weights = TRUE, 
+#                        psu_var = 'clid', strata_var = 'strat', 
+#                        weight_var = 'weight') 
