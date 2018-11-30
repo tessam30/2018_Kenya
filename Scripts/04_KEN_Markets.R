5c47efebd1636d8dd1fc6615fa41f692d6e11e2e
Baboyma Kagniniwa <baboyma@gmail.com>
2018-11-30 00:33:45 -0500

    Starting to look at available commodities in Kenya and its nearby markets.

--- Scripts/04_KEN_Markets.R
+++ Scripts/04_KEN_Markets.R
@@ -6,8 +6,6 @@
 ## Libraries
 library(tidyverse)
 library(sf)
-library(rgdal)
-library(rgeos)
 library(readxl)
 
 
@@ -114,12 +112,76 @@ ken.markets %>%
   geom_sf(data = ken.adm0bOut) +
   geom_sf(data = ken.adm0p, fill = 'white') +
   geom_sf() +
-  ggtitle("KENYA - Commodities Markets") +
+  ggtitle("KENYA - FewsNet Markets") +
   theme_bw()
 
+## Fews Markets Commodities Prices
+fews.prices <- read_excel(paste0(marketspath, "/", m.prices), sheet=1)
 
+str(fews.prices)
+head(fews.prices)
+glimpse(fews.prices)
 
+# filter markets in Kenya
+ken.prices <- fews.prices %>%
+  filter(market_id %in% ken.markets$id)
 
+# look up markets in both datasets
+sort(unique(ken.prices$market))
+sort(unique(ken.markets$market))
 
+# Compare #FALSE => prices dataset has less markets
+# Be-careful here. You might want to lat/long in prices dataset
+#length(unique(ken.prices$market)) == length(unique(ken.markets$market))
 
+## let's repeat selection by location with prices datasets
+str(fews.prices)
+colnames(fews.prices)
+# [1] "geographic_group"               "fewsnet_region"                
+# [3] "fewsnet_region_code"            "country"                       
+# [5] "market"                         "cpcv2"                         
+# [7] "cpcv2_description"              "product"                       
+# [9] "source_organization"            "source_document"               
+# [11] "collection_status"              "period_date"                   
+# [13] "price_type"                     "product_source"                
+# [15] "unit"                           "unit_type"                     
+# [17] "unit_name"                      "currency"                      
+# [19] "status"                         "value"                         
+# [21] "common_unit"                    "common_currency"               
+# [23] "exchange_rate"                  "common_unit_price"             
+# [25] "common_currency_price"          "value_one_month_ago"           
+# [27] "value_one_year_ago"             "value_two_years_ago"           
+# [29] "value_three_years_ago"          "value_four_years_ago"          
+# [31] "value_five_years_ago"           "value_one_year_ahead"          
+# [33] "two_year_average"               "five_year_average"             
+# [35] "pct_change_from_one_month_ago"  "start_date"                    
+# [37] "id"                             "datacollectionperiod"          
+# [39] "datacollection"                 "datasourcedocument"            
+# [41] "datasourceorganization"         "market_id"                     
+# [43] "dataseries"                     "dataseries_name"               
+# [45] "dataseries_specialization_type" "specialization_type"           
+# [47] "created"                        "modified"                      
+# [49] "status_changed"                 "collection_status_changed"     
+# [51] "collection_schedule"            "longitude"                     
+# [53] "latitude"                      
 
+fews.prices <- fews.prices %>%
+  select(id, market_id, market, country, product, product_source, 
+         unit, unit_type, unit_name, currency, exchange_rate, 
+         value, latitude, longitude) %>%
+  filter(!is.na(latitude) & !is.na(longitude)) %>%
+  st_as_sf(coords=c('longitude', 'latitude'), crs=4326) %>%
+  st_transform(3857)
+
+ken.prices <- fews.prices %>%
+  st_intersection(ken.adm0bOut)
+
+ken.prices %>%
+  ggplot() +
+  geom_sf(data = ken.adm0bOut) +
+  geom_sf(data = ken.adm0p, fill='white') +
+  geom_sf() +
+  theme_bw()
+
+# what are the common products?
+sort(unique(ken.prices$product))
