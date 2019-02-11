2339f5fac3ceaa899ecfd45a3a0ca596988af271
Tessam <tim.essam@gmail.com>
2019-02-10 22:48:34 -0500

    added new colors + heatmap

--- 01b_WASH_budget_analysis.R
+++ 01b_WASH_budget_analysis.R
@@ -16,28 +16,40 @@ water_color = "#1d91c0"
 
 
 
- # "#8dd3c7" #green --> Health - 7
- # "#ffffb3" #yellow --> Governor - 2
- # "#bebada" #purple --> Treasury - 3
- # "#fb8072" #red --> Econ growth - 5
- # "#80b1d3" #blue --> Water and Natural Resources - 11
- # "#fdb462" #orange --> Education - 6
- # "#b3de69" #light green --> Agriculture - 9
- # "#fccde5" #pink --> County Assembly - 1
- # "#d9d9d9" #grey --> Governor or County Executive - 2
- # "#bc80bd" #dark purple --> Infrastructure - 4
- # "#ccebc5" #mint green --> Public Service Boards - 12
- # "#ffed6f" #bright yellow --> land housing 8
+# "#fccde5" #pink --> County Assembly - 1
+# "#d9d9d9" #grey --> Governor or County Executive - 2
+# "#bebada" #purple --> Treasury - 3 --> #0F8554 dark green
+# "#bc80bd" #dark purple --> Infrastructure - 4
+# "#fb8072" #red --> Econ growth - 5
+# "#fdb462" #orange --> Education - 6
+# "#8dd3c7",#green --> Health - 7 OR #38A6A5
+# "#ffffb3" #yellow --> Governor - 2
+# "#ffed6f" #bright yellow --> land housing 8
+# "#b3de69" #light green --> Agriculture - 9 #73AF48
+# "#cab2d6" # ---> Youth 10
+# "#80b1d3" #blue --> Water and Natural Resources - 11
+# "#ccebc5" #mint green --> Public Service Boards - 12
+
 
 # Budget colors based on color brewer
  bud_clrs <- c("#fccde5", "#ffffb3", "#bebada", "#bc80bd", 
               "#fb8072", "#fdb462", "#8dd3c7", "#ffed6f",
               "#b3de69", "#cab2d6", "#80b1d3", "#ccebc5")
-
-    
+ #5F4690,#1D6996, 3, 4, 5, #EDAD08,#E17C05,#CC503E,#94346E,#6F4070,#994E95,#666666
+ 
+# Another option for colors from CartoDB -- #855C75,#D9AF6B,#AF6458,#736F4C,#526A83,#625377,#68855C,#9C9C5E,#A06177,#8C785D,#467378,#7C7C7C ; Prism colors
+ cat_colrs = c("#5F4690", "#1D6996", "#38A6A5",
+              "#0F8554", "#73AF48", "#EDAD08",
+              "#E17C05", "#CC503E", "#94346E",
+              "#6F4070", "#994E95", "#666666")
+  
+ 
+# Plot basics
+GC_caption <- c("Source: USAID GeoCenter Calculations from County Government Budget Implementation Review Reports 2015/16, 2016/17, 2017/18")
+ 
 # First, we'll show the total development spending on Water as a total share       
 bs_plot <- function(df, x) {
-  group_size = 12 # This can be adjusted to be calculated automoatically depending on category sizes
+  #group_size = 12 # This can be adjusted to be calculated automoatically depending on category sizes
   ptitle <- budget_cw$Budget_title[budget_cw$`Category Code` == x] # Grab category label for title/file name
   #color = colorRampPalette(RColorBrewer::brewer.pal(9,"Set1"))(group_size)[x] # Grab distinct color for each fill
   color = bud_clrs[x]
@@ -56,10 +68,10 @@ bs_plot <- function(df, x) {
     facet_wrap(~county_sort) + 
     coord_flip() + 
     theme_minimal() +
-    ggtitle(str_c(ptitle, " Spending Compared to Overall Development Expenditures"),
+    ggtitle(str_c(ptitle, " Development Expenditures Compared to Overall Development Expenditures"),
             subtitle = "Percentage indicates share of development spending relative to total development expenditures") +
     labs(y = "Development expenditures in Kshs. Million", x = "",
-         caption = "Source: USAID GeoCenter Calculations from County Government Budget Implementation Review Reports 2015/16, 2016/17, 2017/18") +
+         caption = GC_caption) +
     theme(strip.text = element_text(hjust = 0),
           axis.text = element_text(size = 8),
           axis.title = element_text(size = 8, hjust = 0),
@@ -76,18 +88,31 @@ bs_plot <- function(df, x) {
   b_list <- (seq(1, 12, by = 1))
   map(b_list, ~bs_plot(budget, .))
 
-
+bs_plot(budget, 2)
 
 # Heat map of budget categories across time
-budget %>% select(County, exp_dev_share, budget_year, total_exp_dev, Budget_title) %>%
-  mutate(county_sort = fct_reorder(County, total_exp_dev, .desc = TRUE)) %>% 
-  ggplot(aes(x = county_sort, y = Budget_title, fill = exp_dev_share)) +
-  geom_tile(color = "white") + scale_fill_viridis_c(direction = -1, alpha = 0.75) +
-  theme_minimal() +  coord_fixed(ratio = 1.5) +
-  facet_wrap(~ budget_year, nrow = 3) +
-  theme(legend.position = "none",
-        axis.text.x = element_text(angle = 90,  hjust = 1)) +
-  ggtitle("")
+budget %>% 
+  filter(`Category Code` != 0) %>% 
+  select(County, exp_dev_share, budget_year, total_exp_dev, Budget_title) %>%
+  mutate(county_sort = fct_reorder(County, total_exp_dev, .desc = TRUE),
+         budget_sort = fct_reorder(Budget_title, exp_dev_share, .desc = TRUE)) %>% 
+  ggplot(aes(x = County, y = budget_year)) +
+  geom_tile(aes(fill = exp_dev_share), color = grey80K) + 
+  scale_fill_viridis_c(direction = -1, alpha = 0.90, 
+                       option = "A", label = percent_format(accuracy = 2)) + # format labels in legend
+  theme_minimal() +  
+  coord_fixed(ratio = 1.5) + # Fix the size of the squares
+  facet_wrap(~ budget_sort, ncol = 2) +
+  theme(legend.position = "top",
+        axis.text.x = element_text(angle = 90,  hjust = 1),
+        axis.title = element_text(size = 8, hjust = 0), # making the x-axis title smaller
+        panel.spacing = unit(1.5, "lines")) + # adding more spaced between panels
+  labs(fill = 'Share of Development Expenditures', 
+       y = "", x = "",
+       caption = GC_caption) +
+  ggtitle("Development expenditures on Transport and Infrastructure formed the largest budget share") +
+  ggsave(file.path(imagepath, "KEN_budget_shares_heatmap.pdf"), 
+                   height = 11.7, width = 16.5)
 
 # Stacked bar graph of budget totals over time -- Not that useful
   budget %>% 
@@ -115,21 +140,28 @@ budget %>% select(County, exp_dev_share, budget_year, total_exp_dev, Budget_titl
       ggplot(aes(x = budget_year, y = !!yvar)) +
       geom_line(colour = "grey") +
       geom_point(aes(colour = !!yvar)) +
-      scale_colour_viridis_c(direction = -1) +
+      scale_colour_viridis_c(direction = -1, option = "D") +
       facet_wrap(~ County, nrow = 8) +
       theme_minimal() +
       theme(axis.title.x = element_text(size = 8),
-            legend.position = "none") +
-      scale_y_continuous(labels = scales::percent_format(accuracy = 1), breaks = seq(0, 1, by = 0.25)) +
+            legend.position = "none",
+            panel.spacing = unit(1.5, "lines")) +
+      scale_y_continuous(labels = scales::percent_format(accuracy = 1), 
+                         breaks = seq(0, 1, by = 0.25)) +
       scale_x_continuous(breaks = seq(2015, 2018, by = 1)) +
-      ggtitle(str_c(ptitle, "Absorption Rate")) +
-      labs(y = "", x = "",
+      ggtitle(str_c(ptitle, " Absorption Rate")) +
+      labs(y = "absorption rate", x = "",
            caption = "Source: USAID GeoCenter Calculations from County Government Budget Implementation Review Reports 2015/16, 2016/17, 2017/18") +
         ggsave(file.path(imagepath, str_c(ptitle, " Absorption Rate Summary.pdf")), 
                plot = last_plot(),
                height = 8.5, width = 11, dpi = 300, useDingbats = FALSE)
   }
   
-  b_plot(budget, x = 6)
+  b_plot(budget, x = 4)
   map(b_list, ~b_plot(budget, .))
+  
+
+# Exporting budget data ---------------------------------------------------
+
+write_csv(budget, path = file.path(budgetpath, "KEN_budget_draft_2019_02_10.csv"))  
   
