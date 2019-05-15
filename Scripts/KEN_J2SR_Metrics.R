7a50be017803ca0037d496fbd6f8dec5d9acf0ca
Tessam <tim.essam@gmail.com>
2019-05-14 22:58:43 -0400

    Looking into clustering patterns

--- Scripts/KEN_J2SR_Metrics.R
+++ Scripts/KEN_J2SR_Metrics.R
@@ -7,7 +7,13 @@
 library(tidyverse)
 library(broom)
 library(knitr)
+library(dendextend) # for comparing two dendrograms
 library(ggfortify)
+library(gridExtra)
+library(ggpubr)
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms & visualization
+library(NbClust)  # 30 indices for determining number of clusters
 
 # PCA code modified from https://tbradley1013.github.io/2018/02/01/pca-in-a-tidy-verse-framework/
 
@@ -23,12 +29,12 @@ omit_vars <- c("CID", "Change14-17", "Any_Shock", "Ag_shocks",
 
 j2sr_pca <- 
   j2sr %>% 
-  select(-CID, -Veg_Ch) %>% 
-  #select(-omit_vars) %>% 
+  #select(-CID, -Veg_Ch) %>% 
+  select(-omit_vars) %>% 
   #filter(County != "Nairobi City") %>% 
   nest() %>% 
   mutate(pca = map(data, ~prcomp(.x %>% 
-                                   select(-County), 
+                                 select(-County), 
                                  center = TRUE, 
                                  scale = TRUE)),
          pca_aug = map2(pca, data, ~augment(.x, data = .y)))
@@ -68,23 +74,25 @@ j2sr_pca %>%
                  data = .y, label = TRUE,
                  label.label = "County",
                  label.repel = TRUE) +
-        theme_bw() +
+        theme_minimal() +
         labs(x = "Principal Component 1",
              y = "Principal Component 2",
-             title = "First two principal components of PCA on J2SR dataset")
+             title = "First two principal components of PCA on select variables of J2SR dataset")
     )
   ) %>%
   pull(pca_graph)
 
+ggsave(file.path(imagepath, "KEN_J2SR_pca_results.pdf"),
+       plot = last_plot(),
+       device = "pdf",
+       height = 8.5, width = 11, dpi = 300, 
+       useDingbats = FALSE)
 
 
 # Cluster analysis of countie ---------------------------------------------
-library(cluster)    # clustering algorithms
-library(factoextra) # clustering algorithms & visualization
-library(NbClust)  # 30 indices for determining number of clusters
+
 
 # https://uc-r.github.io/kmeans_clust
-
 
 j2sr_scaled <- 
   j2sr %>%
@@ -96,7 +104,7 @@ str(j2sr_scaled)
 summary(j2sr_scaled)
 
 distance <- get_dist(j2sr_scaled)
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+fviz_dist(distance, gradient = list(low = "#3288bd", mid = "white", high = "#d53e4f"))
 
 # The package has spoken, 3 is the best number of clusters
 res <- NbClust(j2sr_scaled, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10,
@@ -105,23 +113,38 @@ res <- NbClust(j2sr_scaled, diss = NULL, distance = "euclidean", min.nc = 2, max
 
 k3 <- kmeans(j2sr_scaled, centers = 3, nstart = 25)
 
-fviz_cluster(k3, data = j2sr_scaled, repel = TRUE) + theme_minimal() +
+cluster_scatter <- fviz_cluster(k3, data = j2sr_scaled, repel = TRUE) + theme_minimal() +
   scale_colour_manual(values = c("#8dd3c7", "#fb8072", "#80b1d3")) +
   scale_fill_manual(values = c("#8dd3c7", "#fb8072", "#80b1d3")) +
-  theme(legend.position = "top",) +
-  labs(title = "Based on the J2SR data, counties cluster into three major groups",
-       subtitle = "Nairobi City is essentially in a class of its own",
-       caption = "GeoCenter calcuations based on K-means clustering algorithm")
+  theme(legend.position = "top") #+
+  #labs(title = "Based on the J2SR data, counties cluster into three major groups",
+       #subtitle = "Nairobi City is essentially in a class of its own",
+       #caption = "GeoCenter calcuations of  based on K-means clustering algorithm")
+
 
 cluster_df <- k3$cluster %>% 
   tibble::enframe("County", "cluster") %>% 
   mutate(CID = seq(from = 1, to = n(), by = 1))
 
-cluster_df %>% 
+cluster_map <- 
+  cluster_df %>% 
   left_join(., asal_geo, by = c("CID")) %>% 
   ggplot() +
-  geom_sf(aes(fill = factor(cluster)), alpha = 0.60) +
-  scale_fill_manual(values = c("#8dd3c7", "#fb8072", "#80b1d3")) 
+  geom_sf(aes(fill = factor(cluster)), alpha = 0.75, colour = "white") +
+  scale_fill_manual(values = c("#8dd3c7", "#fb8072", "#80b1d3")) +
+  theme_minimal() +
+  theme(legend.position = "off")
+
+cluster_plot <- ggarrange(cluster_map, cluster_scatter, ncol = 2) %>% 
+  #annotate_figure(., fig.lab = "Turkana and Kitui, on average, had the most humanitarian caseloads from 2004 - 2018")
+  annotate_figure(., fig.lab = "K-means cluster analysis divides the counties into three major groups based on the J2SR proposed variables")
+
+
+  ggsave(file.path(imagepath, "KEN_J2SR_cluster_analysis.pdf"),
+         plot = cluster_plot,
+         device = "pdf",
+         height = 8.5, width = 11, dpi = 300, 
+         useDingbats = FALSE)
 
 
 
@@ -149,7 +172,26 @@ print(gap_stat, method = "firstmax")
 fviz_gap_stat(gap_stat)
 
 
+# Hierarchical clustering -------------------------------------------------
+
+# https://uc-r.github.io/hc_clustering
+
+d <- dist(j2sr_scaled, method = "euclidean")
+
+# Hierarchical clustering using Complete Linkage
+hc1 <- hclust(d, method = "complete" )
+
+# Plot the obtained dendrogram
+plot(hc1, cex = 0.6, hang = -1)
 
 
+# DIANA method
+# compute divisive hierarchical clustering
+hc4 <- diana(j2sr_scaled)
 
+# Divise coefficient; amount of clustering structure found
+hc4$dc
+
+# plot dendrogram
+pltree(hc4, cex = 0.6, hang = -1, main = "Dendrogram of diana")
 
